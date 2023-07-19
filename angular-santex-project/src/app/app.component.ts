2ca875d6556103c29c26135a7532fd92097bde7f
Martin Fernandez <fernandezmartin1534@gmail.com>
2023-07-18 21:33:24 -0300

    Terminando ejercicio angular

--- angular-santex-project/src/app/app.component.ts
+++ angular-santex-project/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  nombre: string;
+  costo: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-santex-project';
+  items: Item[] = [
+    { nombre: 'Producto 1', costo: '$10' },
+    { nombre: 'Producto 2', costo: '$20' },
+    { nombre: 'Producto 3', costo: '$15' },
+  ];
+
+  showList: boolean = true;
+
+  toggleList() {
+    this.showList = !this.showList;
+  }
 }
+
--- angular-santex-project/src/app/app.module.ts
+++ angular-santex-project/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
